18b518ffc8324d5425d642f2efaec1ee2867fc49
Satyendra <satyendradhamgaye2@gmail.com>
2026-02-28 14:11:27 +0530

    more updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,22 @@
 import { Component, signal } from '@angular/core';
+import { SideNav } from './layout/side-nav/side-nav';
+import { TopNavbar } from './layout/top-navbar/top-navbar';
+import { Hero } from './sections/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ SideNav, TopNavbar, Hero],
   templateUrl: './app.html',
+  template: `
+    <div class="app-container">
+      <app-side-nav></app-side-nav>
+
+      <div class="main-content">
+        <app-top-navbar></app-top-navbar>
+        <app-hero></app-hero>
+      </div>
+    </div>
+  `,
   styleUrl: './app.scss'
 })
 export class App {
--- src/app/sections/hero/hero.ts
+++ src/app/sections/hero/hero.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal, ElementRef, inject } from '@angular/core';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true,
+  imports: [NgStyle],
   templateUrl: './hero.html',
-  styleUrl: './hero.scss',
+  styleUrl: './hero.scss'
 })
 export class Hero {
 
+  private el = inject(ElementRef);
+
+  scrollProgress = signal(0);
+
+  @HostListener('window:scroll')
+  onScroll() {
+    const rect = this.el.nativeElement.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    // how much hero has moved upward
+    const progress = Math.min(
+      Math.max((windowHeight - rect.top) / (windowHeight + rect.height), 0),
+      1
+    );
+
+    this.scrollProgress.set(progress);
+  }
+
+  get imageStyle() {
+    const speedMultiplier = 2;   // >1 = faster than text
+    const translate = this.scrollProgress() * 500 * speedMultiplier;
+
+    return {
+      transform: `translateY(${-translate}px)`
+    };
+  }
 }
